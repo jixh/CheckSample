1a08acc71f76da36537254b2eeb7bbcc1f45030c
jktaihe <zxcvbJKnm12>
2017-10-22 11:52:35 +0800

    Initial commit

---
+++ check/src/main/java/com/jktaihe/check/Check.java
@@ -0,0 +1,12 @@
+package com.jktaihe.check;
+
+/**
+ * Created by jktaihe on 22/10/17.
+ * blog: blog.jktaihe.com
+ */
+
+public class Check {
+
+
+
+}
---
+++ check/src/main/java/com/jktaihe/check/CheckManager.java
@@ -0,0 +1,9 @@
+package com.jktaihe.check;
+
+/**
+ * Created by jktaihe on 22/10/17.
+ * blog: blog.jktaihe.com
+ */
+
+public class CheckManager {
+}
